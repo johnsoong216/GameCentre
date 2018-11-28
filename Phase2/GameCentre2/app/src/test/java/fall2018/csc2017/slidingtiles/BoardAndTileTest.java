177a87a26c3d6837108f7f25d7df19c87c2bd9d1
1003898446 <yining.ding@mail.utoronto.ca>
2018-11-27 22:31:19 -0500

    boardManagerUnitTest

--- Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/slidingtiles/BoardAndTileTest.java
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/slidingtiles/BoardAndTileTest.java
@@ -10,92 +10,249 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see < a href=" ">Testing documentation</ a>
  */
 public class BoardAndTileTest {
 
     /**
      * The board manager for testing.
      */
-    BoardManager boardManager;
+    BoardManager boardManager3;
+    BoardManager boardManager4;
+    BoardManager boardManager5;
+
+    List<Tile> tiles3;
+    List<Tile> tiles4;
+    List<Tile> tiles5;
 
     /**
      * Make a set of tiles that are in order.
      *
      * @return a set of tiles that are in order
      */
-    private List<Tile> makeTiles() {
-        List<Tile> tiles = new ArrayList<>();
-        final int numTiles = Board.NUM_ROWS * Board.NUM_COLS;
+    private List<Tile> makeTiles3() {
+        List<Tile> tiles3 = new ArrayList<>();
+        final int numTiles = 9;
         for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-            tiles.add(new Tile(tileNum + 1, tileNum));
+            tiles3.add(new Tile(tileNum + 1, tileNum));
         }
+        return tiles3;
+    }
 
-        return tiles;
+    private List<Tile> makeTiles4() {
+        List<Tile> tiles4 = new ArrayList<>();
+        final int numTiles = 16;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles4.add(new Tile(tileNum + 1, tileNum));
+        }
+        return tiles4;
+    }
+
+    private List<Tile> makeTiles5() {
+        List<Tile> tiles5 = new ArrayList<>();
+        final int numTiles = 25;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles5.add(new Tile(tileNum + 1, tileNum));
+        }
+        return tiles5;
     }
 
     /**
      * Make a solved Board.
      */
     private void setUpCorrect() {
-        List<Tile> tiles = makeTiles();
-        Board board = new Board(tiles);
-        boardManager = new BoardManager(board);
+        tiles3 = makeTiles3();
+        tiles4 = makeTiles4();
+        tiles5 = makeTiles5();
+
+        Board board5 = new Board(tiles5, 5);
+        Board board4 = new Board(tiles4, 4);
+        Board board3 = new Board(tiles3, 3);
+
+        boardManager3 = new BoardManager(board3);
+        boardManager4 = new BoardManager(board4);
+        boardManager5 = new BoardManager(board5);
+
     }
 
-    /**
-     * Shuffle a few tiles.
-     */
-    private void swapFirstTwoTiles() {
-        boardManager.getBoard().swapTiles(0, 0, 0, 1);
+    private void swapFirstTwoTiles3() {
+        boardManager3.getBoard().swapTiles(0, 0, 0, 1);
+    }
+
+    private void swapFirstTwoTiles4() {
+        boardManager4.getBoard().swapTiles(0, 0, 0, 1);
+    }
+
+    private void swapFirstTwoTiles5() {
+        boardManager5.getBoard().swapTiles(0, 0, 0, 1);
     }
 
     /**
      * Test whether swapping two tiles makes a solved board unsolved.
      */
     @Test
-    public void testIsSolved() {
+    public void testIsSolved4() {
         setUpCorrect();
-        assertEquals(true, boardManager.puzzleSolved());
-        swapFirstTwoTiles();
-        assertEquals(false, boardManager.puzzleSolved());
+        assertEquals(true, boardManager4.puzzleSolved());
+        swapFirstTwoTiles4();
+        assertEquals(false, boardManager4.puzzleSolved());
+    }
+
+    @Test
+    public void testIsSolved3() {
+        setUpCorrect();
+        assertEquals(true, boardManager3.puzzleSolved());
+        swapFirstTwoTiles3();
+        assertEquals(false, boardManager3.puzzleSolved());
+    }
+
+    @Test
+    public void testIsSolved5() {
+        setUpCorrect();
+        assertEquals(true, boardManager5.puzzleSolved());
+        swapFirstTwoTiles5();
+        assertEquals(false, boardManager5.puzzleSolved());
     }
 
     /**
      * Test whether swapping the first two tiles works.
      */
     @Test
-    public void testSwapFirstTwo() {
+    public void testSwapFirstTwo4() {
         setUpCorrect();
-        assertEquals(1, boardManager.getBoard().getTile(0, 0).getId());
-        assertEquals(2, boardManager.getBoard().getTile(0, 1).getId());
-        boardManager.getBoard().swapTiles(0, 0, 0, 1);
-        assertEquals(2, boardManager.getBoard().getTile(0, 0).getId());
-        assertEquals(1, boardManager.getBoard().getTile(0, 1).getId());
+        assertEquals(1, boardManager4.getBoard().getTile(0, 0).getId());
+        assertEquals(2, boardManager4.getBoard().getTile(0, 1).getId());
+        boardManager4.getBoard().swapTiles(0, 0, 0, 1);
+        assertEquals(2, boardManager4.getBoard().getTile(0, 0).getId());
+        assertEquals(1, boardManager4.getBoard().getTile(0, 1).getId());
     }
 
-    /**
-     * Test whether swapping the last two tiles works.
-     */
     @Test
-    public void testSwapLastTwo() {
+    public void testSwapFirstTwo3() {
         setUpCorrect();
-        assertEquals(15, boardManager.getBoard().getTile(3, 2).getId());
-        assertEquals(16, boardManager.getBoard().getTile(3, 3).getId());
-        boardManager.getBoard().swapTiles(3, 3, 3, 2);
-        assertEquals(16, boardManager.getBoard().getTile(3, 2).getId());
-        assertEquals(15, boardManager.getBoard().getTile(3, 3).getId());
+        assertEquals(1, boardManager3.getBoard().getTile(0, 0).getId());
+        assertEquals(2, boardManager3.getBoard().getTile(0, 1).getId());
+        boardManager3.getBoard().swapTiles(0, 0, 0, 1);
+        assertEquals(2, boardManager3.getBoard().getTile(0, 0).getId());
+        assertEquals(1, boardManager3.getBoard().getTile(0, 1).getId());
     }
 
+    @Test
+    public void testSwapFirstTwo5() {
+        setUpCorrect();
+        assertEquals(24, boardManager5.getBoard().getTile(4, 3).getId());
+        assertEquals(25, boardManager5.getBoard().getTile(4, 4).getId());
+        boardManager5.getBoard().swapTiles(4, 3, 4, 4);
+        assertEquals(25, boardManager5.getBoard().getTile(4, 3).getId());
+        assertEquals(24, boardManager5.getBoard().getTile(4, 4).getId());
+    }
+    @Test
+    public void testSwapLastTwo3() {
+        setUpCorrect();
+        assertEquals(8, boardManager3.getBoard().getTile(2, 1).getId());
+        assertEquals(9, boardManager3.getBoard().getTile(2, 2).getId());
+        boardManager3.getBoard().swapTiles(2, 1, 2, 2);
+        assertEquals(9, boardManager3.getBoard().getTile(2, 1).getId());
+        assertEquals(8, boardManager3.getBoard().getTile(2, 2).getId());
+    }
+    @Test
+    public void testSwapLastTwo4() {
+        setUpCorrect();
+        assertEquals(15, boardManager4.getBoard().getTile(3, 2).getId());
+        assertEquals(16, boardManager4.getBoard().getTile(3, 3).getId());
+        boardManager4.getBoard().swapTiles(3, 2, 3, 3);
+        assertEquals(16, boardManager4.getBoard().getTile(3, 2).getId());
+        assertEquals(15, boardManager4.getBoard().getTile(3, 3).getId());
+    }
+    @Test
+    public void testSwapLastTwo5() {
+        setUpCorrect();
+        assertEquals(24, boardManager5.getBoard().getTile(4, 3).getId());
+        assertEquals(25, boardManager5.getBoard().getTile(4, 4).getId());
+        boardManager5.getBoard().swapTiles(4, 3, 4, 4);
+        assertEquals(25, boardManager5.getBoard().getTile(4, 3).getId());
+        assertEquals(24, boardManager5.getBoard().getTile(4, 4).getId());
+    }
     /**
      * Test whether isValidHelp works.
      */
     @Test
-    public void testIsValidTap() {
+    public void testIsValidTap4() {
         setUpCorrect();
-        assertEquals(true, boardManager.isValidTap(11));
-        assertEquals(true, boardManager.isValidTap(14));
-        assertEquals(false, boardManager.isValidTap(10));
+        assertEquals(true, boardManager4.isValidTap(11));
+        assertEquals(true, boardManager4.isValidTap(14));
+        assertEquals(false, boardManager4.isValidTap(10));
+    }
+
+    @Test
+    public void testIsValidTap3() {
+        setUpCorrect();
+        assertEquals(true, boardManager3.isValidTap(7));
+        assertEquals(true, boardManager3.isValidTap(5));
+        assertEquals(false, boardManager3.isValidTap(4));
+    }
+
+    @Test
+    public void testIsValidTap5() {
+        setUpCorrect();
+        assertEquals(true, boardManager5.isValidTap(23));
+        assertEquals(true, boardManager5.isValidTap(19));
+        assertEquals(false, boardManager5.isValidTap(18));
+    }
+
+    @Test
+    public void testGetMovements3() {
+        setUpCorrect();
+        boardManager3.getBoard().swapTiles(2, 2, 2, 1);
+        boardManager3.getMovements().push(7);
+        boardManager3.getMovements().push(8);
+        assertEquals(8, boardManager3.getMovements().pop());
+        assertEquals(7, boardManager3.getMovements().pop());
+    }
+
+    @Test
+    public void testGetMovements4() {
+        setUpCorrect();
+        boardManager4.getBoard().swapTiles(3, 3, 3, 2);
+        boardManager4.getMovements().push(14);
+        boardManager4.getMovements().push(15);
+        assertEquals(15, boardManager4.getMovements().pop());
+        assertEquals(14, boardManager4.getMovements().pop());
+    }
+
+    @Test
+    public void testGetMovements5() {
+        setUpCorrect();
+        boardManager5.getBoard().swapTiles(4, 4, 4, 3);
+        boardManager5.getMovements().push(24);
+        boardManager5.getMovements().push(25);
+        assertEquals(25, boardManager5.getMovements().pop());
+        assertEquals(24, boardManager5.getMovements().pop());
+
+    }
+
+    @Test
+    public void testGetBlank3() {
+        setUpCorrect();
+        assertEquals(8, boardManager3.getBlank(tiles3));
+
+
+    }
+
+    @Test
+    public void testGetBlank4() {
+        setUpCorrect();
+        assertEquals(15, boardManager4.getBlank(tiles4));
+
+
+    }
+
+    @Test
+    public void testGetBlank5() {
+        setUpCorrect();
+        assertEquals(24, boardManager5.getBlank(tiles5));
+
+
     }
 }
 
