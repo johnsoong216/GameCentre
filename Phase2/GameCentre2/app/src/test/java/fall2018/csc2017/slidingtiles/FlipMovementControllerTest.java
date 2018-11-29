061747848aae3a9c86f213bb82694e2937dfec5c
1003898446 <yining.ding@mail.utoronto.ca>
2018-11-29 17:27:39 -0500

    flipmovementcontroller

---
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/slidingtiles/FlipMovementControllerTest.java
@@ -0,0 +1,46 @@
+package fall2018.csc2017.slidingtiles;
+
+import android.content.Context;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class FlipMovementControllerTest {
+    FlipMovementController mController = new FlipMovementController();
+    FlipManager flipManager;
+    List<Tile> tiles;
+    private Context context;
+
+    private List<Tile> makeTiles() {
+        List<Tile> tiles = new ArrayList<>();
+        final int numTiles = 14;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(tileNum + 1, tileNum));
+        }
+        tiles.add(new Tile(15));
+        tiles.add(new Tile(14));
+        return tiles;
+    }
+
+    private void setUp() {
+        tiles = makeTiles();
+        FlipIt flip = new FlipIt(tiles, 4);
+        flipManager = new FlipManager(flip);
+        flipManager.puzzleSolved();
+    }
+
+    @Test
+    public void testSetBoardmanager() {
+        setUp();
+        mController.setFlipManager(flipManager);
+        assertEquals(flipManager, mController.getFlipManager());
+    }
+
+    
+
+}
