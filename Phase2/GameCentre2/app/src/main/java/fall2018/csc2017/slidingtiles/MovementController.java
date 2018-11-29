2f3540389e54b1891ead6a46c6b3ac18c628abb3
Jeffrey Chen <ziyu.chen@mail.utoronto.ca>
2018-11-29 17:41:29 -0500

    Created an abstract movement controller and made the 2 controller classes for 2 games subclasses of that class

--- Phase2/GameCentre2/app/src/main/java/fall2018/csc2017/slidingtiles/MovementController.java
+++ Phase2/GameCentre2/app/src/main/java/fall2018/csc2017/slidingtiles/MovementController.java
@@ -1,4 +1,14 @@
 package fall2018.csc2017.slidingtiles;
 
-public interface MovementController {
+import android.content.Context;
+
+abstract class MovementController {
+    GameManager gameManager;
+
+    abstract void setManager(GameManager manager);
+
+    abstract GameManager getManager();
+
+    abstract void processTapMovement(Context context, int position);
+
 }
