21f648e4376a40317afdf1ebaa73683b510f58a3
1003898446 <yining.ding@mail.utoronto.ca>
2018-11-28 20:41:00 -0500

    levels

---
+++ Phase2/GameCentre2/app/src/main/java/fall2018/csc2017/slidingtiles/LevelComplexityActivity.java
@@ -0,0 +1,75 @@
+package fall2018.csc2017.slidingtiles;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class LevelComplexityActivity extends AppCompatActivity {
+
+    private FlipManager flipManager = new FlipManager(5, 3);
+    private ImageButton levelNumbersBtn;
+    private int level;
+    private Context context;
+    private String username;
+    private Loadsave loadsaveManager;
+    private int difficulty = 3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_complexity);
+        context = this;
+        loadsaveManager = new Loadsave(context);
+        username = Session.getCurrentUser().getUsername();
+        levelNumbersBtn = this.findViewById(R.id.levelNumbersBtn);
+        chooseGameLevel();
+        addStartNumberTiles();
+    }
+
+    private void chooseGameLevel() {
+        SeekBar levelSeekBar = findViewById(R.id.levelSeekBar);
+        levelSeekBar.setProgress(0);
+        levelSeekBar.incrementProgressBy(1);
+        levelSeekBar.setMax(2);
+        Log.d("ABC", "difficulty" + difficulty);
+        levelSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar levelSeekBar, int levelProgress, boolean levelFromUser) {
+                level = levelProgress + 1;
+                int difficulty = getIntent().getIntExtra("difficulty", 0);
+                flipManager = new FlipManager(difficulty, level);
+                flipManager.getFlip().setNUM_ROWS(difficulty);
+                flipManager.getFlip().setNUM_COLS(difficulty);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar levelSeekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar levelSeekBar) {
+                Toast.makeText(LevelComplexityActivity.this, "level" + level + " Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addStartNumberTiles() {
+        levelNumbersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int undo = getIntent().getIntExtra("undo", 0);
+                flipManager.setUndo(undo);
+                loadsaveManager.saveToFile(StartingActivity.TEMP_SAVE_FILE, username, "flip_it", flipManager);
+                Intent startGame = new Intent(LevelComplexityActivity.this, FlipGameActivity.class);
+                startActivity(startGame);
+            }
+        });
+    }
+}
+
