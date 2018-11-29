ab9b94e728e8204e20b3ed046d8bbd68d801c2d7
yefengka <fengkai.ye@mail.utoronto.ca>
2018-11-29 12:54:14 -0500

    java doc and more unit test for blackjack

---
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/slidingtiles/BlackJackGameTest.java
@@ -0,0 +1,43 @@
+package fall2018.csc2017.slidingtiles;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlackJackGameTest {
+
+    /*
+    Create deck and blackjack game for test
+     */
+    Deck deck1;
+    Deck deck2;
+    BlackJackGame blackJackGame1;
+    BlackJackGame blackJackGame2;
+
+    private void setUpDeck(){
+
+    }
+    @Test
+    public void getDealerPoint() {
+    }
+
+    @Test
+    public void getPlayerPoint() {
+    }
+
+    @Test
+    public void dealerDrawCard() {
+    }
+
+    @Test
+    public void playerDrawCard() {
+    }
+
+    @Test
+    public void getBet() {
+    }
+
+    @Test
+    public void inGameBet() {
+    }
+}
