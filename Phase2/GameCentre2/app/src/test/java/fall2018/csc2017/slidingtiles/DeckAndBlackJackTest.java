54893fb498a7d0b079afa9cfb780ef87dff99fb9
yefengka <fengkai.ye@mail.utoronto.ca>
2018-11-28 23:50:34 -0500

    final version, still need to refactor

--- Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/slidingtiles/DeckAndBlackJackTest.java
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/slidingtiles/DeckAndBlackJackTest.java
@@ -11,28 +11,47 @@ public class DeckAndBlackJackTest {
     Deck deck1;
     Deck deck2;
     Deck deck3;
+    Deck deck4;
     Card card1;
     Card card2;
     Hand hand1;
     Hand hand2;
+    Hand hand3;
     private void setUpDeck(){
         deck1 = new Deck();
         deck2 = new Deck();
         deck3 = new Deck();
+        deck4 = new Deck();
         card1 = new Card(3, 13);
         card2 = new Card(3, 3);
         hand1 = new Hand();
         hand2 = new Hand();
+        hand3 = new Hand();
         deck2.setNotShuffled();
         deck3.setNotShuffled();
+        deck4.setNotShuffled();
         for(int i = 0; i < 10; i++){
             deck3.popCard();
         }
+        for(int i = 0; i < 12; i++){
+            deck4.popCard();
+        }
+    }
+    /*
+    hand1 contains 2 cards with in game points of 20
+    hand2 contains 2 cards with in game poins of 21 which is blackjack
+     */
+    private void setUpHand(){
+        setUpDeck();
+        hand1.drawCard(deck2);
+        hand1.drawCard(deck2);
+        hand2.drawCard(deck4);
+        hand2.drawCard(deck2);
+        hand3.drawCard(deck2);
     }
     @Test
     public void testPopCard1() {
         setUpDeck();
-//        assertEquals(52, deck2.popCard().getId());
         assertEquals(card1, deck2.popCard());
     }
 
@@ -72,8 +91,61 @@ public class DeckAndBlackJackTest {
     }
 
     @Test
-    public void testDrawCard1AndGet() {
-        setUpDeck();
+    public void testDrawCard1AndGet1() {
+        setUpHand();
+        assertEquals(21, hand2.getPoints());
+    }
+    @Test
+    public void testDrawCard1AndGet2() {
+        setUpHand();
+        assertEquals(20, hand1.getPoints());
+    }
+
+    @Test
+    public void testGetHandSize() {
+        setUpHand();
+        assertEquals(2, hand1.getHandSize());
+    }
+
+    @Test
+    public void testGoBusted() {
+        setUpHand();
         hand1.drawCard(deck2);
+        assertEquals(true, hand1.goBusted());
+    }
+
+    @Test
+    public void testCheckBlackJack() {
+        setUpHand();
+        assertEquals(true, hand2.checkBlackJack());
+        assertEquals(false, hand1.checkBlackJack());
+    }
+
+    @Test
+    public void testFirstAce(){
+        setUpHand();
+        assertEquals(true, hand2.checkFirstAce());
+        assertEquals(false, hand1.checkFirstAce());
+    }
+
+    @Test
+    public void testGetBackGround(){
+        setUpHand();
+        assertEquals(R.drawable.bj50, hand2.getCardBackGround(1));
+    }
+
+    @Test
+    public void testCheckDouble(){
+        setUpHand();
+        hand3.getPoints();
+        assertEquals(true, hand3.isAllowDouble());
+    }
+
+    @Test
+    public void testHandIterator(){
+        setUpHand();
+        for(Card card : hand1){
+            assertEquals(10, card.getInGameValue());
+        }
     }
 }
