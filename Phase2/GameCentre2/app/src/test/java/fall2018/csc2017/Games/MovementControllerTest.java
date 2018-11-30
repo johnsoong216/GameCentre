c94553bae026659b9082117781cf53a0feab1f84
Jeffrey Chen <ziyu.chen@mail.utoronto.ca>
2018-11-30 03:14:48 -0500

    Deleted unnecessary imports

--- Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/Games/MovementControllerTest.java
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/Games/MovementControllerTest.java
@@ -3,8 +3,6 @@ package fall2018.csc2017.Games;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class MovementControllerTest {
     MovementController mController = new MovementController();
