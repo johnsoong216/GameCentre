ca835d1c30cc8604b4a5b609428b7d18dd34b172
Jeffrey Chen <ziyu.chen@mail.utoronto.ca>
2018-11-30 00:49:57 -0500

    Updated the tests

--- Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/Games/MovementControllerTest.java
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/Games/MovementControllerTest.java
@@ -10,19 +10,11 @@ import static org.mockito.Mockito.verify;
 
 public class MovementControllerTest {
     MovementController mController = new MovementController();
-    GameManager boardManager = new BoardManager(4);
+    BoardManager boardManager = new BoardManager(4);
 
     @Test
     public void testGetManager() {
         mController.setManager(boardManager);
         assertEquals(boardManager, mController.getManager());
     }
-
-//    @Test
-//    public void testProcessTap() {
-//        FlipManager flipManager = mock(FlipManager.class);
-//        Context context = mock(Context.class);
-//        mController.processTapMovement(context, 5);
-//        verify(flipManager.touchColor(5));
-//    }
 }
