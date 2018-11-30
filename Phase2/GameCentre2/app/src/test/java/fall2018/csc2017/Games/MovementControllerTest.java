8834d10a6b3d80d45c2396c262cd357c1724698f
Jeffrey Chen <ziyu.chen@mail.utoronto.ca>
2018-11-30 04:26:25 -0500

    Updated the version

--- Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/Games/MovementControllerTest.java
+++ Phase2/GameCentre2/app/src/test/java/fall2018/csc2017/Games/MovementControllerTest.java
@@ -15,23 +15,6 @@ public class MovementControllerTest {
     MovementController mController = new MovementController();
     SlidingTileBoardManager boardManager = new SlidingTileBoardManager(4);
 
-    private List<Tile> makeTiles() {
-        List<Tile> tiles = new ArrayList<>();
-        final int numTiles = 16;
-        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
-            tiles.add(new Tile(tileNum + 1, tileNum));
-        }
-
-        return tiles;
-    }
-
-    private void setUp() {
-        List<Tile> tiles = makeTiles();
-        SlidingTileBoard board = new SlidingTileBoard(tiles, 4);
-        boardManager = new SlidingTileBoardManager(board);
-
-    }
-
     @Test
     public void testGetManager() {
         mController.setManager(boardManager);
